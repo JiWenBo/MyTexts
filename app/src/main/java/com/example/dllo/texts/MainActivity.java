a7687d6e38a3f45ed95559e588195a7b76f76bc8
nan <1104352824@qq.com>
2016-03-28 21:06:49 +0800

    信息完成

--- app/src/main/java/com/example/dllo/texts/MainActivity.java
+++ app/src/main/java/com/example/dllo/texts/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//立刻就暗示的绿卡就算了看得见你啦；上午
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
