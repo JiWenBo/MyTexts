901f3b8cd480130b7c9381e6821903a9a36dab71
dllo <Jwb9325@163.com>
2016-03-28 21:43:40 +0800

    按钮界面

---
+++ app/src/main/java/com/example/dllo/texts/NewActivity.java
@@ -0,0 +1,15 @@
+package com.example.dllo.texts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dllo on 16/3/28.
+ */
+public class NewActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newactivity);
+    }
+}
