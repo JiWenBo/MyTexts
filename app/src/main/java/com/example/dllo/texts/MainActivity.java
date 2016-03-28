d00d795fc7a416f06ba3586dbf692a2168cf479b
李亚辉 <969898528@qq.com>
2016-03-28 21:06:33 +0800

    注册

--- app/src/main/java/com/example/dllo/texts/MainActivity.java
+++ app/src/main/java/com/example/dllo/texts/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //网络完成
+        //注册
     }
 }
